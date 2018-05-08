b0c7b7236c3d5595499029bf74b31d1fa0166c74
bolboa <ebolboa@gmail.com>
2018-05-08 18:04:41 -0400

    boilerplate

---
+++ src/components/Page1/Page1.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from "./Page1.css"
+
+const Page1 = () => (
+  <div>
+    <p className={style.para}>Page 1</p>
+  </div>
+)
+
+export default Page1
---
+++ src/components/Page2/Page2.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import style from "./Page2.css"
+
+
+const Page2 = () => (
+  <div>
+    <p className={style.para}>Page 2</p>
+  </div>
+)
+
+export default Page2
